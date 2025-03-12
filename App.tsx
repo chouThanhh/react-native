52d3e52cea2e61e408fe6622c92a5fd8df54f203
tuhoc.cc <ksgoku2004@gmail.com>
2025-03-12 13:17:27 +0700

    14 sd Fonts

--- App.tsx
+++ App.tsx
@@ -1,145 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { Alert, Button, FlatList, Keyboard, Pressable, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native';
-import FlexBox from './components/flex.box';
-import AntDesign from '@expo/vector-icons/AntDesign';
+import { View, Text } from "react-native"
+import HomeScreen from "./components/review/home";
+import DetailScreen from "./components/review/detail";
+import AboutScreen from "./components/review/about";
 
-interface ITodo {
-  id: number;
-  name: string
-}
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
 
-export default function App() {
+SplashScreen.preventAutoHideAsync();
 
-  const [todo, setTodo] = useState("");
-
-  const [listTodo, setListTodo] = useState<ITodo[]>([])
-
-  // { id: 1, name: "watching hoidanit" }
-
-  function randomInteger(min: number, max: number) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  const handleAddTodo = () => {
-    if (!todo) {
-      Alert.alert("Lỗi input todo", "Todo không được dể trống",
-        [
-          {
-            text: 'Hủy',
-            onPress: () => console.log('Cancel Pressed'),
-            style: 'cancel',
-          },
-          {
-            text: 'Xác nhận',
-            onPress: () => console.log('OK Pressed')
-          }
-        ]
-      )
-      return;
+const App = () => {
+  const [loaded, error] = useFonts({
+    'OpenSans-Regular': require('./assets/fonts/OpenSans-Regular.ttf'),
+  });
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
     }
-    setListTodo([...listTodo,
-    { id: randomInteger(2, 2000), name: todo }
-    ])
-    setTodo("")
+  }, [loaded, error]);
+  if (!loaded && !error) {
+    return null;
   }
 
-  const deleteTodo = (id: number) => {
-    const newTodos = (listTodo.filter(item => item.id !== id))
-    setListTodo(newTodos)
-  }
-  //jsx
   return (
-    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-      <View style={styles.container}>
-        {/* header */}
-        <Text style={styles.header}>Todo App</Text>
-
-        {/* form  */}
-        <View style={styles.form}>
-          <TextInput
-            value={todo}
-            style={styles.todoInput}
-            onChangeText={(value) => setTodo(value)}
-          />
-          <Button title='Add to do'
-            onPress={handleAddTodo}
-          />
-        </View>
-
-        {/* list todo */}
-        <View style={styles.todo}>
-          <FlatList
-            data={listTodo}
-            keyExtractor={item => item.id + ""}
-            renderItem={({ item }) => {
-              return (
-                <Pressable
-                  onPress={() => deleteTodo(item.id)}
-                  style={({ pressed }) => ({ opacity: pressed ? 0.5 : 1 })}
-                >
-                  <View style={styles.groupTodo}>
-                    <Text
-                      style={styles.todoItem}>{item.name}</Text>
-                    <AntDesign name="close" size={24} color="black" />
-                  </View>
-                </Pressable>
-              )
-            }}
-          />
-        </View>
-
-      </View >
-    </TouchableWithoutFeedback>
-  );
+    <View>
+      <HomeScreen />
+      <DetailScreen />
+      <AboutScreen />
+    </View>
+  )
 }
 
-//css in js
-const styles = StyleSheet.create({
-  groupTodo: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    borderWidth: 1,
-    borderStyle: "dashed",
-    marginBottom: 15,
-    padding: 15,
-    marginHorizontal: 10
-  },
-  header: {
-    backgroundColor: 'orange',
-    paddingHorizontal: 20,
-    textAlign: 'center',
-    fontSize: 60,
-  },
-  form: {
-    // flex: 2
-    marginBottom: 20
-  },
-  todo: {
-    flex: 1
-  },
-  container: {
-    paddingTop: 50,
-    borderWidth: 1,
-    borderColor: "red",
-    flex: 1,
-    backgroundColor: '#fff',
-
-  },
-  todoInput: {
-    borderBottomWidth: 1,
-    borderBottomColor: "green",
-    padding: 5,
-    margin: 15,
-  },
-  body: {
-    paddingHorizontal: 10,
-    marginBottom: 20,
-    flex: 1
-  },
-  todoItem: {
-    fontSize: 30,
-    // marginBottom: 20,
-    // padding: 10
-  }
-});
+export default App;
---
+++ components/review/about.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native"
+
+
+const AboutScreen = () => {
+    return (
+        <View>
+            <Text style={{ fontSize: 30 }}>About Screen google</Text>
+        </View>
+    )
+}
+
+export default AboutScreen
