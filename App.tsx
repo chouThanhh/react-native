338c2df8d056e4613ebbb217f0059f75390fcb34
tuhoc.cc <ksgoku2004@gmail.com>
2025-03-12 15:21:02 +0700

    15 gioi thieu navigaion

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native"
+import { View, Text, StyleSheet } from "react-native"
 import HomeScreen from "./components/review/home";
 import DetailScreen from "./components/review/detail";
 import AboutScreen from "./components/review/about";
@@ -6,12 +6,13 @@ import AboutScreen from "./components/review/about";
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
+import { OPENSANS_REGULAR } from "./utils/const";
 
 SplashScreen.preventAutoHideAsync();
 
 const App = () => {
   const [loaded, error] = useFonts({
-    'OpenSans-Regular': require('./assets/fonts/OpenSans-Regular.ttf'),
+    [OPENSANS_REGULAR]: require('./assets/fonts/OpenSans-Regular.ttf'),
   });
   useEffect(() => {
     if (loaded || error) {
@@ -23,12 +24,18 @@ const App = () => {
   }
 
   return (
-    <View>
+    <View style={styles.container}>
       <HomeScreen />
       <DetailScreen />
       <AboutScreen />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 30
+  }
+})
 
 export default App;
